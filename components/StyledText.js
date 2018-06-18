f5289a8115f655c42c4e6e89e3ed82b84c27cebd
Guizmo <martinezguillaum@gmail.com>
2018-06-18 15:32:11 +0200

    Push test

--- components/StyledText.js
+++ components/StyledText.js
@@ -3,7 +3,7 @@ import { Text } from 'react-native';
 
 export class MonoText extends React.Component {
   render() {
-    const { style, color } = this.props;
+    const { style, color, fontSize } = this.props;
     return (
       <Text {...this.props} style={[style, { fontFamily: 'space-mono' }, color && { color }]} />
     );
